d0657ceccdf797f8126aac4e97603affe6cb0d66
Мария <114505489+MariAntonova94@users.noreply.github.com>
2023-03-18 13:07:51 +0700

    Add files via upload

---
+++ VendingMachines.java
@@ -0,0 +1,9 @@
+package org.example.OOP.seminar1;
+
+
+public interface VendingMachines {
+
+    abstract void acceptingMoney();
+    abstract void productDelivery();
+    abstract void deliveryOfChange();
+}
